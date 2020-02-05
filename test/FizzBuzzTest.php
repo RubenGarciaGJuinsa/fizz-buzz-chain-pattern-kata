af799d6a2cda96c126c7a940c5510857750bad10
Rubén García <rubengarcia@grupojuinsa.es>
2020-02-05 13:43:42 +0100

    RED: add pass_one_get_one test

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -1,14 +1,17 @@
 <?php
 namespace Test;
 
+use Kata\FizzBuzz;
 use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
     /** @test */
     public function
-    dummy_test()
+    pass_one_get_one()
     {
-        $this->assertTrue(true);
+        $fizzBuzz = new FizzBuzz();
+        $response = $fizzBuzz->print(1);
+        $this->assertEquals('1', $response);
     }
 }
